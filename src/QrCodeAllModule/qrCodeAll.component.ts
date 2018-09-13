9f9ae2caea2b8bc06d98ca7d24f62fbbef548792
nileskh2504 <nileshfbk123@gmail.com>
2018-09-13 23:39:31 +0530

    Scan QR code functionality is now implemented

--- src/QrCodeAllModule/qrCodeAll.component.ts
+++ src/QrCodeAllModule/qrCodeAll.component.ts
@@ -1,12 +1,36 @@
-import { Component, OnInit, OnChanges, ViewChild, Input, ElementRef, Renderer2 } from '@angular/core';
-declare var require: any;
-const QrCode = require('qrcode');
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'qr-code-all',
-    template: '<div [class]="qrCodeClass" #qrCodeDiv></div>'
+    template: `
+    <h1>Scan or Create QR Codes in your applications</h1>
+    <ng-container [ngSwitch]="scanQrCode">
+    <ng-container *ngSwitchDefault>
+        <app-qrcode-create [qrCodeType]="qrCodeType"
+        [qrCodeVersion]="qrCodeVersion"
+        [qrCodeStyle]="qrCodeClass"
+        [width]="width"
+        [margin]="margin"
+        [scale]="scale"
+        [qrCodeECLevel]="qrCodeECLevel"
+        [qrCodeValue]="qrCodeValue"
+        [qrCodeColorLight]="qrCodeColorLight"
+        [qrCodeColorDark]="qrCodeColorDark">
+        </app-qrcode-create>
+    </ng-container>
+    <ng-container *ngSwitchCase="true">
+        <app-qrcode-scan [canvasWidth]="canvasWidth"
+        [canvasHeight]="canvasHeight"
+        [debug]="debug"
+        [stopAfterScan]="stopAfterScan"
+        [updateTime]="updateTime"
+        (onCapture)="emitDecodedData($event)">
+        </app-qrcode-scan>
+    </ng-container>
+</ng-container>
+    `
 })
-export class QrCodeAllComponent implements OnInit, OnChanges {
+export class QrCodeAllComponent implements OnInit {
 
     @Input('qrCodeType') qrCodeType: 'url' | 'img' | 'canvas' = 'url';
     @Input('qrCodeVersion') qrCodeVersion: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23' | '24' | '25' | '26' | '27' | '28' | '29' | '30' | '31' | '32' | '33' | '34' | '35' | '36' | '37' | '38' | '39' | '40' | '' = '';
@@ -18,98 +42,47 @@ export class QrCodeAllComponent implements OnInit, OnChanges {
     @Input('qrCodeValue') qrCodeValue: string = 'SK2504 is the best in the world';
     @Input('qrCodeColorLight') qrCodeColorLight: string = '#ffffff';
     @Input('qrCodeColorDark') qrCodeColorDark: string = '#000000';
-    @ViewChild('qrCodeDiv') qrCodeDiv: ElementRef;
 
-    constructor(private renderer: Renderer2) {
+    @Input('canvasWidth') canvasWidth: number = 640;
+    @Input('canvasHeight') canvasHeight: number = 480;
+    @Input('debug') debug: boolean = false;
+    @Input('stopAfterScan') stopAfterScan: boolean = true;
+    @Input('updateTime') updateTime: number = 500;
+    @Input('scanQrCode') scanQrCode: boolean = false;
+
+    @Output('onCapture') onCapture = new EventEmitter();
+
+    constructor() {
 
     }
 
     ngOnInit() {
-
+        this.setDefaultValues();
     }
 
-    ngOnChanges() {
-        this.produceQrCode();
+    setDefaultValues() {
+        if(this.notSafe(this.qrCodeType)) this.qrCodeType = 'url';
+        if(this.notSafe(this.qrCodeVersion)) this.qrCodeVersion = '';
+        if(this.notSafe(this.qrCodeClass)) this.qrCodeClass = 'qrcodecss';
+        if(this.notSafe(this.width)) this.width = 11;
+        if(this.notSafe(this.margin)) this.margin = 4;
+        if(this.notSafe(this.qrScale)) this.qrScale = 4;
+        if(this.notSafe(this.qrCodeECLevel)) this.qrCodeECLevel = 'M';
+        if(this.notSafe(this.qrCodeValue)) this.qrCodeValue = 'SK2504 is the best in the world';
+        if(this.notSafe(this.qrCodeColorLight)) this.qrCodeColorLight = '#ffffff';
+        if(this.notSafe(this.qrCodeColorDark)) this.qrCodeColorDark = '#000000';
+        if(this.notSafe(this.canvasWidth)) this.canvasWidth = 640;
+        if(this.notSafe(this.canvasHeight)) this.canvasHeight = 480;
+        if(this.notSafe(this.stopAfterScan)) this.stopAfterScan = true;
+        if(this.notSafe(this.updateTime)) this.updateTime = 500;
     }
 
-    produceQrCode() {
-        if(this.qrCodeValue) {
-            switch(this.qrCodeType) {
-                case 'canvas':
-                    this.createQrCanvas();
-                    break;
-                case 'img':
-                case 'url':
-                default:
-                    this.createQrUrlImage();
-                    break;
-            }
-        } else {
-            return;
-        }
+    notSafe(variable: any): boolean {
+        return (variable === null || variable === undefined);
     }
 
-    createQrCanvas() {
-        const canvasEl: HTMLCanvasElement = this.renderer.createElement('canvas');
-        this.toCanvasQr(canvasEl).then((data) => {
-            this.renderElement(canvasEl);
-        }).catch((error) => {
-            console.error(error);
-        });
+    emitDecodedData(decoded: string) {
+        this.onCapture.emit(decoded);
     }
-
-    createQrUrlImage() {
-        const imgEl: HTMLImageElement = this.renderer.createElement('img');
-        this.toImgUrlQr().then((url: string) => {
-            imgEl.setAttribute('src', url);
-            this.renderElement(imgEl);
-        }).catch((err) => {
-            console.error(err);
-        });
-    }
-
-    toCanvasQr(canvasEl: HTMLCanvasElement) {
-        return new Promise((resolve, reject) => {
-            QrCode.toCanvas(canvasEl, this.qrCodeValue, {
-                version: this.qrCodeVersion,
-                errorCorrectionLevel: this.qrCodeECLevel,
-                width: this.width,
-                margin: this.margin,
-                scale: this.qrScale,
-                color: {
-                    light: this.qrCodeColorLight,
-                    dark: this.qrCodeColorDark
-                }
-            }, (error: any) => {
-                error ? reject(error) : resolve('success');
-            });
-        });
-    }
-
-    toImgUrlQr() {
-        return new Promise((resolve, reject) => {
-            QrCode.toDataURL(this.qrCodeValue, {
-                version: this.qrCodeVersion,
-                errorCorrectionLevel: this.qrCodeECLevel,
-                width: this.width,
-                margin: this.margin,
-                scale: this.qrScale,
-                color: {
-                    light: this.qrCodeColorLight,
-                    dark: this.qrCodeColorDark
-                }
-            }, (error: any, url: string) => {
-                error ? reject(error) : resolve(url);
-            });
-        });
-    }
-
-    renderElement(element: any) {
-        if(this.qrCodeDiv) {
-            for(let child of this.qrCodeDiv.nativeElement.childNodes) {
-                this.renderer.removeChild(this.qrCodeDiv.nativeElement, child);
-            }
-            this.renderer.appendChild(this.qrCodeDiv.nativeElement, element);
-        }
-    }
+    
 }
